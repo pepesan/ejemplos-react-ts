ca6488759e74c042c67dd628574bebc3a12d2774
pepesan <pepesan@gmail.com>
2020-03-02 12:20:10 +0100

    integrado primer componente

---
+++ src/MiComponente.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class  MiComponente extends Component{
+    render(): React.ReactElement{
+        return (
+            <div id="MiComponente">
+                <h2>Mi Componente</h2>
+            </div>
+        )
+    }
+}
+
+export default MiComponente;
