461c367e3a4651eb7e8aac3dece5827616aa42c7
David Vaquero <pepesan@gmail.com>
2020-06-29 19:01:48 +0200

    inclusión de componente que usa componentes para probar props y states

---
+++ src/AppComponentes.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropiedadesComponente from "./PropiedadesComponente";
+function AppComponentes() {
+  return (
+    <div className="App">
+        <PropiedadesComponente name="pepe" age="12" email="p@p.com"/>
+    </div>
+  );
+}
+
+export default AppComponentes;
