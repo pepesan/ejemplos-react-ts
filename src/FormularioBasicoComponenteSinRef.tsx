3af30bb2e468ca192f878c37311dd2a2e4cbc726
David Vaquero <pepesan@gmail.com>
2020-06-30 16:28:25 +0200

    ejemplo de uso de formulario con nuevo uso de refs

--- src/FormularioBasicoComponenteSinRef.tsx
+++ src/FormularioBasicoComponenteSinRef.tsx
@@ -1,23 +1,25 @@
 import React, {Component, FormEvent, createRef} from 'react';
 
 class  FormularioBasicoComponenteSinRef extends Component<any, any>{
-    private myRef = createRef<HTMLInputElement>()
+    private myRef: React.RefObject<HTMLInputElement>;
     constructor(props: Readonly<any>) {
         super(props);
         this.state = {value: ''};
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.myRef = createRef();
 
     }
 
     handleChange(event: FormEvent) {
         //const name = (this.refs['name'] as any as HTMLInputElement).value.trim();
-        console.log(this.myRef.current);
-        //this.setState({value: name});
+        console.log(this.myRef);
+        this.setState({value: (this.myRef.current as HTMLInputElement).value});
     }
 
     handleSubmit(event: FormEvent) {
         console.log('A name was submitted: ' + this.state.value);
+        console.log(this.myRef.current);
         event.preventDefault();
     }
     render(): React.ReactElement{
@@ -31,7 +33,8 @@ class  FormularioBasicoComponenteSinRef extends Component<any, any>{
                             type="text"
                             value={this.state.value}
                             ref={this.myRef}
-                            onChange={this.handleChange} />
+                            onChange={this.handleChange}
+                        />
                     </label>
                     <input type="submit" value="Submit" />
                 </form>
